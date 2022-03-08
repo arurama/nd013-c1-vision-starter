0f3f1e77183f732ee0e30ea60dc64a469a18de65
Arun <arurama@gmail.com>
2022-03-09 00:24:31 +0530

    function add on

--- create_splits.py
+++ create_splits.py
@@ -1,11 +1,14 @@
 import argparse
 import glob
 import os
-import random
 
-import numpy as np
+import shutil
 
+from sklearn.model_selection import train_test_split
 from utils import get_module_logger
+
+
+
 
 
 def split(source, destination):
@@ -18,7 +21,36 @@ def split(source, destination):
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     # TODO: Implement function
+    files= glob.glob(source + '\\*.tfrecord')
 
+    File_train, File_val_test = train_test_split( files, train_size=0.6, random_state=45)
+    File_val, File_test = train_test_split( File_val_test, train_size=0.5, random_state=45)
+
+
+    TrainFolder = os.path.join(destination, 'train')
+    TestFolder = os.path.join(destination, 'test')
+    ValFolder = os.path.join(destination, 'val')
+
+    # make dir 
+    if not os.path.exists(TrainFolder):
+        os.makedirs(TrainFolder)
+        
+    if not os.path.exists(TestFolder):
+        os.makedirs(TestFolder)
+
+    if not os.path.exists(ValFolder):
+        os.makedirs(ValFolder)
+        
+
+    for File in File_train :
+        shutil.move( File, TrainFolder )
+        
+    for File in File_val :
+        shutil.move(File, ValFolder )
+        
+    for File in File_test :
+        shutil.move(File, TestFolder)
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
